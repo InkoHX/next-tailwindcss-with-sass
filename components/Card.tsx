a29e08643747f9323be25802a15e1a3b487aabd1
InkoHX <me@inkohx.dev>
2023-05-31 01:18:49 +0900

    add example code

---
+++ components/Card.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styles from "../styles/Card.module.scss";
+
+export interface CardProps {
+  children: React.ReactNode;
+  title: string;
+}
+
+export const Card: React.FC<CardProps> = (props) => {
+  return (
+    <div className={styles.Card}>
+      <div className={styles.Card__title}>{props.title}</div>
+      <div className={styles.Card__description}>{props.children}</div>
+    </div>
+  );
+};
